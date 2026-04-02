ae65dee9c1e42b4562582f64d45a29de34546140
Sam Guglielmino <saguglielmino04@gmail.com>
2026-04-01 23:04:02 -0400

    UI Updates
    Made changes to UI flow control. May or may not work as intended.

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,36 +1,105 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Panels")]
     public GameObject mainPanel;
     public GameObject obtainingDataPanel;
+    public GameObject successPanel;
+    public GameObject authFailurePanel;
+    public GameObject enrollFailurePanel;
+    public GameObject backUpCreationPanel;
+    public GameObject backUpPanel;
+
+    [Header("Obtaining Data UI")]
+    public Slider progressBar;
+    public GameObject continueButton;
+    public float progressDuration = 3f;
+
     void Start()
     {
-        mainPanel.SetActive(true);
-        obtainingDataPanel.SetActive(false);
+        ShowOnly(mainPanel);
     }
+
+    void Update() { }
+
+    // Panel navigation
 
     public void OpenObtainingData()
     {
-        mainPanel.SetActive(false);
-        obtainingDataPanel.SetActive(true);
+        ShowOnly(obtainingDataPanel);
+        continueButton.SetActive(false);
+        StartCoroutine(RunProgressBar());
     }
+
     public void BackToMain()
     {
-        mainPanel.SetActive(true);
-        obtainingDataPanel.SetActive(false);
+        ShowOnly(mainPanel);
+    }
+
+    public void ShowSuccess()
+    {
+        ShowOnly(successPanel);
+    }
+
+    public void ShowAuthFailure()
+    {
+        ShowOnly(authFailurePanel);
+    }
+
+    public void ShowEnrollFailure()
+    {
+        ShowOnly(enrollFailurePanel);
+    }
+
+    public void ShowBackUpCreation()
+    {
+        ShowOnly(backUpCreationPanel);
+    }
+
+    public void ShowBackUp()
+    {
+        ShowOnly(backUpPanel);
     }
 
     public void CloseAll()
     {
-        mainPanel.SetActive(false);
-        obtainingDataPanel.SetActive(false);
+        ShowOnly(null);
     }
-    // Update is called once per frame
-    void Update()
+
+    // Progress Bar
+    private IEnumerator RunProgressBar()
     {
-        
+        progressBar.value = 0f;
+        float elapsed = 0f;
+
+        while (elapsed < progressDuration)
+        {
+            elapsed += Time.deltaTime;
+            progressBar.value = Mathf.Clamp01(elapsed / progressDuration);
+            yield return null;
+        }
+
+        progressBar.value = 1f;
+        continueButton.SetActive(true);
+    }
+
+    // Utility
+    private void ShowOnly(GameObject target)
+    {
+        GameObject[] allPanels = {
+            mainPanel, obtainingDataPanel, successPanel,
+            authFailurePanel, enrollFailurePanel,
+            backUpCreationPanel, backUpPanel
+        };
+
+        foreach (GameObject panel in allPanels)
+        {
+            if (panel != null)
+                panel.SetActive(panel == target);
+        }
     }
 }
